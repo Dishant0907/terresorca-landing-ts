f1a1e7925813f38f299dd382a8bdb34207294a70
Dishant <trydishant2002@gmail.com>
2024-03-07 22:26:34 +0530

    heading text color fixed in partner comp

--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -34,7 +34,7 @@ export const Partner = () => {
   return (
 
     <>
-    <h2 className='text-3xl font-bold text-center mt-[7rem]'>Our Partners</h2>
+    <h2 className='text-3xl font-bold text-center text-white mt-[7rem]'>Our Partners</h2>
     <div className='m-[3rem]  flex justify-center'>
         <InfiniteMovingCards
         items={brands}
